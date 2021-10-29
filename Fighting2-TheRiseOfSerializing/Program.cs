66bf71ae77b4012f5ac1532568267e2609afda80
Samuel Palmér <samiskramis@gmail.com>
2021-10-29 12:11:39 +0200

    Initial Commit

---
+++ Fighting2-TheRiseOfSerializing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fighting2_TheRiseOfSerializing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+
+    public class Player
+    {
+        public string name { get; set; }
+        public string hp { get; set; }
+        public string attack { get; set; }
+        public string hitssChance { get; set; }
+    }
+}

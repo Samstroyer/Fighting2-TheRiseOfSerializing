78f1a9f0442c80ac224e12ae4d53e16691a7e980
Samuel Palmér <samiskramis@gmail.com>
2021-10-30 21:42:29 +0200

    Copy this

--- Fighting2-TheRiseOfSerializing/Program.cs
+++ Fighting2-TheRiseOfSerializing/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text.Json;
 
 namespace Fighting2_TheRiseOfSerializing
 {
@@ -6,16 +8,26 @@ namespace Fighting2_TheRiseOfSerializing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string rawPlayerData = File.ReadAllText(@"..\players.json");
+            PlayerCollection deserializedPlayerData = JsonSerializer.Deserialize<PlayerCollection>(rawPlayerData);
 
+            foreach (Player a in deserializedPlayerData.players)
+            {
+                System.Console.WriteLine(a.name);
+            }
         }
+    }
+
+    public class PlayerCollection
+    {
+        public Player[] players { get; set; }
     }
 
     public class Player
     {
         public string name { get; set; }
-        public string hp { get; set; }
-        public string attack { get; set; }
-        public string hitssChance { get; set; }
+        public string hp { get; }
+        public string attack { get; }
+        public string hitChance { get; }
     }
 }
